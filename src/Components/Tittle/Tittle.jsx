9c384de52e2de9103f07ed45d0d58c204ac1face
ignajo <ignacio.villagrah@gmail.com>
2024-04-14 21:23:10 -0400

    "first commit, creacion de componentes"

---
+++ src/Components/Tittle/Tittle.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Tittle/Tittle.css'
+
+const Tittle = () => {
+  return (
+    <div className='tittle'>
+      <p>OUR PROGRAM</p>
+      <h2>What we Offer</h2>
+    </div>
+  )
+}
+
+export default Tittle
